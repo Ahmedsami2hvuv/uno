122df9bdaa77c61ce25cb5c3c9d90080f4fb9673
Ahmedsami2hvuv <reozaki.1211@gmail.com>
2026-02-05 01:44:36 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+DB_URL = os.getenv("DATABASE_URL")
+IMG_CW = os.getenv("IMG_CW")
+IMG_CCW = os.getenv("IMG_CCW")
+
