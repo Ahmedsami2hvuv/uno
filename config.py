5d40f7184544961c836f5fb9bffba8ae45343e68
Ahmedsami2hvuv <reozaki.1211@gmail.com>
2026-02-05 02:16:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from aiogram import Bot
 
 TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
@@ -7,3 +8,5 @@ DB_URL = os.getenv("DATABASE_URL")
 IMG_CW = os.getenv("IMG_CW")
 IMG_CCW = os.getenv("IMG_CCW")
 
+# تعريف البوت هنا ليكون متاحاً للكل
+bot = Bot(token=TOKEN)
