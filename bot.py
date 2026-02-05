9cdf600143429fed5473780732d8f224393205eb
Ahmedsami2hvuv <reozaki.1211@gmail.com>
2026-02-05 17:23:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,19 +8,17 @@ from handlers import common, calc, online, stats, admin
 logging.basicConfig(level=logging.INFO)
 
 async def main():
-    print("⏳ جاري التشغيل...")
     init_db()
-    
     dp = Dispatcher()
-
-    # ربط جميع الملفات
+    
+    # ربط المجلدات
     dp.include_router(common.router)
     dp.include_router(calc.router)
     dp.include_router(online.router)
     dp.include_router(stats.router)
     dp.include_router(admin.router)
 
-    print("🚀 البوت يعمل الآن بدون أخطاء!")
+    print("🚀 البوت انطلق بنجاح!")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
