05221098d8d92d47128e9e98299c7540ac367782
Ahmedsami2hvuv <reozaki.1211@gmail.com>
2026-02-05 15:39:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,22 @@
 import os
 from aiogram import Bot
 
-TOKEN = os.getenv("BOT_TOKEN")
+# 1. الإعدادات الأساسية
+TOKEN = os.getenv("BOT_TOKEN") # تأكد إذا كان الاسم في ريلوي BOT_TOKEN أو TOKEN
 ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 DB_URL = os.getenv("DATABASE_URL")
+
+# 2. صور نظام اتجاه اللعب (الحاسبة)
 IMG_CW = os.getenv("IMG_CW")
 IMG_CCW = os.getenv("IMG_CCW")
+
+# 3. صور نظام الأونو (إضافة جديدة)
+# وضعنا "123" كقيمة افتراضية لكي يشتغل البوت وتستخرج الأكواد براحتك
+IMG_UNO_SAFE_ME = os.getenv("IMG_UNO_SAFE_ME", "123")
+IMG_UNO_SAFE_OPP = os.getenv("IMG_UNO_SAFE_OPP", "123")
+IMG_CATCH_SUCCESS = os.getenv("IMG_CATCH_SUCCESS", "123")
+IMG_CATCH_PENALTY = os.getenv("IMG_CATCH_PENALTY", "123")
 
 # تعريف البوت
 bot = Bot(token=TOKEN)
